a817e80c57897e19ce3bb3f65d7ffcc57683de1c
Jaqueline <jaquelinenacarate@gmail.com>
2023-09-13 09:41:29 +0000

    Define new class method authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates_uniqueness_of :email, case_sensitive: false
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.strip.downcase) # Remove leading/trailing spaces and make email case-insensitive
+    if @user && @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
+
 end
