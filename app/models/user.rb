a87b0880779bc7438fc2c9065b9a3af555b615f5
Jaqueline <jaquelinenacarate@gmail.com>
2023-09-13 09:37:48 +0000

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   
   has_secure_password
 
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
+  validates :password, presence: true, length: { minimum: 6 }
 end
